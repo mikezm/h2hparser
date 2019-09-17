a31e1c0c44768956bf2a030e70142e3711b7d7dd
mikezm <mmarshall@reachmail.com>
2019-09-17 03:01:12 -0500

    updated endpoint path after api refactoring.

--- bridge/__init__.py
+++ bridge/__init__.py
@@ -1,6 +1,4 @@
 import os, base64, json
-#from urllib.request import Request, urlopen
-#from urllib.parse import urlencode
 import requests
 
 def make_request(url=None, data=None, headers=None):
@@ -26,7 +24,7 @@ pwd = os.environ.get('H2H_ADMIN_PWD')
 
 auth_string = bytes(eml + ':' + pwd, 'utf-8')
 basic_header = { 'Authorization': 'Basic ' + base64.b64encode(auth_string).decode() }
-auth_data = make_request(url=base_uri+'/auth/login', headers=basic_header)
+auth_data = make_request(url=base_uri+'/user/login', headers=basic_header)
 token_header = { 'Authorization': 'Bearer ' + auth_data['data']['token'] }
 
 
